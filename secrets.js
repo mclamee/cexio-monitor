23b284b718c7e93845ad2feb75dc07648f01eee1
William Zhang <williamz@synnex.com>
2018-01-23 00:12:25 -0800

    initial check in

---
+++ secrets.js
@@ -0,0 +1,4 @@
+// please add your secrets here, they are generated on the API page of CEX.IO
+exports.userName = "up1111111111",
+exports.apiKey = "7zGBk8LyTm0VrsB0l6ktmyMZto",
+exports.apiSecret = "UOjETUKlv1Nyh3PfKuRdDYm2Xx"
